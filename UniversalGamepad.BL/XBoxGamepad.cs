5063f5f6902279c7118ef2441f97bcc505391d17
Tom Demets <tom_demets@outlook.com>
2015-12-07 22:10:37 +0100

    First stable version

---
+++ UniversalGamepad.BL/XBoxGamepad.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Gaming.Input;
+
+namespace UniversalGamepad.BL
+{
+    public class XBoxGamepad
+    {
+        private List<Gamepad> _controllers = new List<Gamepad>();
+        private bool _running = true;
+
+        Task backgroundWorkTask;
+
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressA;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressB;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressX;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressY;
+
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressLeft;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressRight;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressUp;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonPressDown;
+
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonMenu;
+        public event EventHandler<GamepadButtons> OnXBoxGamepadButtonView;
+
+        public event EventHandler<List<Double>> OnXBoxGamepadLeftStickMove;
+        public event EventHandler<List<Double>> OnXBoxGamepadRightStickMove;
+
+
+
+        public XBoxGamepad()
+        {
+            Gamepad.GamepadAdded += Gamepad_GamepadAdded;
+            Gamepad.GamepadRemoved += Gamepad_GamepadRemoved;
+            backgroundWorkTask = Task.Run(() => PollGamepad());
+        }
+
+        public async Task Vibrate()
+        {
+            if (_running)
+            {
+                foreach (Gamepad controller in _controllers)
+                {
+                    GamepadVibration gv = new GamepadVibration();
+                    gv.LeftMotor = 1;
+                    gv.RightMotor = 1;
+                    controller.Vibration = gv;
+                    await Task.Delay(1000);
+                    gv.LeftMotor = 0;
+                    gv.RightMotor = 0;
+                    controller.Vibration = gv;
+                }
+            }
+        }
+        
+        private void Start()
+        {
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            _running = false;
+        }
+
+        public async Task PollGamepad()
+        {
+            while (true)
+            {
+                if (_running)
+                {
+                    foreach (Gamepad controller in _controllers)
+                    {
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.A)
+                        {
+                            OnXBoxGamepadButtonPressA(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.B)
+                        {
+                            OnXBoxGamepadButtonPressB(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.X)
+                        {
+                            OnXBoxGamepadButtonPressX(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.Y)
+                        {
+                            OnXBoxGamepadButtonPressY(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.DPadLeft)
+                        {
+                            OnXBoxGamepadButtonPressLeft(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.DPadUp)
+                        {
+                            OnXBoxGamepadButtonPressUp(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.DPadRight)
+                        {
+                            OnXBoxGamepadButtonPressRight(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.DPadDown)
+                        {
+                            OnXBoxGamepadButtonPressDown(controller, controller.GetCurrentReading().Buttons);
+                        }
+
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.Menu)
+                        {
+                            OnXBoxGamepadButtonMenu(controller, controller.GetCurrentReading().Buttons);
+                        }
+                        if (controller.GetCurrentReading().Buttons == GamepadButtons.View)
+                        {
+                            OnXBoxGamepadButtonView(controller, controller.GetCurrentReading().Buttons);
+                        }
+
+                        if (controller.GetCurrentReading().LeftThumbstickX >= 0.1
+                            || controller.GetCurrentReading().LeftThumbstickX <= -0.1
+                            || controller.GetCurrentReading().LeftThumbstickY >= 0.1
+                            || controller.GetCurrentReading().LeftThumbstickY <= -0.1)
+                        {
+                            OnXBoxGamepadLeftStickMove(controller, new List<double>() { controller.GetCurrentReading().LeftThumbstickX, controller.GetCurrentReading().LeftThumbstickY });
+                        }
+                        if (controller.GetCurrentReading().RightThumbstickX >= 0.1 
+                            || controller.GetCurrentReading().RightThumbstickX <= -0.1
+                            || controller.GetCurrentReading().RightThumbstickY >= 0.1
+                            || controller.GetCurrentReading().RightThumbstickY <= -0.1)
+                        {
+                            OnXBoxGamepadRightStickMove(controller, new List<double>() { controller.GetCurrentReading().RightThumbstickX, controller.GetCurrentReading().RightThumbstickY });
+                        }
+                    }
+                }
+                await Task.Delay(50);
+            }
+        }
+
+        private void Gamepad_GamepadRemoved(object sender, Gamepad e)
+        {
+            _controllers.Remove(e);
+        }
+
+        private void Gamepad_GamepadAdded(object sender, Gamepad e)
+        {
+            _controllers.Add(e);
+        }
+    }
+}
---
+++ UniversalGamepad.GUI/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Gaming.Input;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UniversalGamepad.GUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        UniversalGamepad.BL.XBoxGamepad gamepad;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            gamepad = new BL.XBoxGamepad();
+            gamepad.OnXBoxGamepadButtonPressA += Gamepad_OnXBoxGamepadButtonPressA;
+            gamepad.OnXBoxGamepadButtonPressB += Gamepad_OnXBoxGamepadButtonPressB;
+            gamepad.OnXBoxGamepadButtonPressX += Gamepad_OnXBoxGamepadButtonPressX;
+            gamepad.OnXBoxGamepadButtonPressY += Gamepad_OnXBoxGamepadButtonPressY;
+
+            gamepad.OnXBoxGamepadLeftStickMove += Gamepad_OnXBoxGamepadLeftStickMove;
+            gamepad.OnXBoxGamepadRightStickMove += Gamepad_OnXBoxGamepadRightStickMove;
+
+            gamepad.OnXBoxGamepadButtonMenu += Gamepad_OnXBoxGamepadButtonMenu;
+            gamepad.OnXBoxGamepadButtonView += Gamepad_OnXBoxGamepadButtonView;
+        }
+
+        public async void UpdateOutput(string message)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                lblOutput.Text = String.Format("{0}{1}{2}", message, Environment.NewLine, lblOutput.Text);
+            }
+            );
+        }
+
+        private void Gamepad_OnXBoxGamepadButtonView(object sender, GamepadButtons e)
+        {
+            UpdateOutput(e.ToString());
+        }
+
+        private void Gamepad_OnXBoxGamepadButtonMenu(object sender, GamepadButtons e)
+        {
+            UpdateOutput(e.ToString());
+        }
+
+        private void Gamepad_OnXBoxGamepadRightStickMove(object sender, List<double> e)
+        {
+            UpdateOutput(e[0].ToString() + ":" + e[1].ToString());
+        }
+
+        private void Gamepad_OnXBoxGamepadLeftStickMove(object sender, List<double> e)
+        {
+            UpdateOutput(e[0].ToString() + ":" + e[1].ToString());
+        }
+
+        private void Gamepad_OnXBoxGamepadButtonPressY(object sender, GamepadButtons e)
+        {
+            UpdateOutput(e.ToString());
+        }
+
+        private async void Gamepad_OnXBoxGamepadButtonPressX(object sender, GamepadButtons e)
+        {
+            UpdateOutput(e.ToString());
+            await gamepad.Vibrate();
+        }
+
+        private void Gamepad_OnXBoxGamepadButtonPressB(object sender, GamepadButtons e)
+        {
+            UpdateOutput(e.ToString());
+        }
+
+        private void Gamepad_OnXBoxGamepadButtonPressA(object sender, GamepadButtons e)
+        {
+            UpdateOutput(e.ToString());
+        }
+        
+    }
+}
